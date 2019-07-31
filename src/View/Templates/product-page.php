863491d5cbca1d1afea01944fd41ccb258f53f91
Boros Dori <dorottya.boros@evozon.com>
2019-07-31 18:32:56 +0300

    modify product template

--- src/View/Templates/product-page.php
+++ src/View/Templates/product-page.php
@@ -2,3 +2,17 @@
 <?php $product = $data['product']; ?>
 
 <?php echo $product->getId(); ?>
+
+<div class="container">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="<?= $product->getThumbnailPath() ?>" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">
+                Product title: <?= $product->getTitle() ?>
+            </h5>
+            <p class="card-text">
+                Product description:  <?= $product->getDescription() ?>
+            </p>
+        </div>
+    </div>
+</div>
